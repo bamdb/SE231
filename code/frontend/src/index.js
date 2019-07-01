b1327d0cad0de92fbddc7c2051675b9ee6c7de6e
shenruien <40877197+scarletfantasy@users.noreply.github.com>
2019-07-01 15:28:05 +0800

    tag,userinfo,item finish

--- code/frontend/src/index.js
+++ code/frontend/src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Userinfo from'./component/userinfo';
+import Scheduletable from'./component/scheduletable';
+import Tag from'./component/tag';
+import Item from'./component/item';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Tag/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
