29285fc4288188e9b7574fecada26e9b1ad7f6cd
YuyangHuang <50946857+YuyangHuang@users.noreply.github.com>
2019-07-02 23:22:51 +0800

    7.2 finish

--- code/frontend/src/index.js
+++ code/frontend/src/index.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
-import Login from './login';
+import Login from './component/login';
+import App from './App';
+import Userinfo from './component/userinfo';
+import Scheduletable from './component/scheduletable';
+import Tag from './component/tag';
+import Item from './component/item';
 import * as serviceWorker from './serviceWorker';
-import Register from "./register";
+import Register from "./component/register";
 
 
 class Home extends Component{
@@ -22,6 +27,8 @@ class Home extends Component{
 }
 
 ReactDOM.render(<Home />, document.getElementById('root'));
+
+//ReactDOM.render(<Scheduletable/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
