182cd54e669ae33e1ed4663f393be42d0a7d6c48
YuyangHuang <50946857+YuyangHuang@users.noreply.github.com>
2019-07-04 10:07:04 +0800

    page1

--- code/frontend/src/component/homepage.js
+++ code/frontend/src/component/homepage.js
@@ -3,18 +3,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Navigation from "./navigation";
+import TopItemList from "./topitemlist";
+import Browserlist from "./browserlist";
 
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        minWidth: 500,
-        display: "flex",
-        flexWrap: "nowrap",
-        justifyContent: "space-around",
-        height: 50,
-    },
+    root:{
+        width: "20%",
+    }
 }));
-
+/*
 class TopPart extends Component{
     constructor(props){
         super(props);
@@ -24,19 +22,14 @@ class TopPart extends Component{
     render(){
         return(
             <Grid container className={useStyles.root} >
-                <Grid item xs={3}>
-                    <Paper></Paper>/* image of "Bamdb" */
-                </Grid>
                 <Grid item xs={12}>
                     <Navigation />
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper></Paper>
                 </Grid>
             </Grid>
         )
     }
 }
+ */
 
 class HomePage extends Component{
     constructor(props){
@@ -46,7 +39,17 @@ class HomePage extends Component{
 
     render(){
         return(
-            <TopPart></TopPart>
+            <Grid container >
+                <Grid> <Navigation /> </Grid>
+                <Grid container direction={"row"} >
+                    <Grid className={useStyles.root}>
+                        <TopItemList />
+                    </Grid>
+                    <Grid>
+                        <Browserlist />
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
