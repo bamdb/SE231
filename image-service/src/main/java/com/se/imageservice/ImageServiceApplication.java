ec840771dcf0a7de24e6125caad209125da401d6
isalb729 <34480825+isalb729@users.noreply.github.com>
2019-07-04 23:52:56 -0700

    annotation

--- image-service/src/main/java/com/se/imageservice/ImageServiceApplication.java
+++ image-service/src/main/java/com/se/imageservice/ImageServiceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 public class ImageServiceApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/se/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/se/userservice/UserServiceApplication.java
@@ -2,11 +2,13 @@ package com.se.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
