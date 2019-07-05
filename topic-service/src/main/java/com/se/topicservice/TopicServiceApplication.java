a40da857b8e29e17f490166e71d0442102cc5fd2
pleiadesian <wzl574402791@outlook.com>
2019-07-05 09:09:40 +0800

    initialize topic service

---
+++ topic-service/src/main/java/com/se/topicservice/TopicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.se.topicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicServiceApplication.class, args);
+    }
+
+}
