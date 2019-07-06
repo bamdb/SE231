69c48e13d08c25852aca81ab8fea36a0047ba613
pleiadesian <wzl574402791@outlook.com>
2019-07-06 19:35:08 +0800

    init rating service

---
+++ rating-service/src/main/java/com/se/ratingservice/RatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.se.ratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingServiceApplication.class, args);
+    }
+
+}
