7f5819f9bb4141969e21bb3849cc5afcdf2f12dc
isalb729 <34480825+isalb729@users.noreply.github.com>
2019-07-08 02:12:08 -0700

    change uri

--- image-service/src/main/java/com/se/imageservice/ImageRepository.java
+++ image-service/src/main/java/com/se/imageservice/ImageRepository.java
@@ -6,8 +6,8 @@ import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface ImageRepository extends CrudRepository<Image, String> {
-    Optional<Image> findByImageId(Long imageId);
+    Optional<Image> findByImageId(Long bookId);
     @Transactional
-    void deleteByImageId(Long imageId);
+    void deleteByImageId(Long bookId);
     Boolean existsByImageId(Long itemId);
 }
