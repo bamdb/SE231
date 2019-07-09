3fd1eb05e689dec5c15df2862a33eda1f2b329ea
shenruien <40877197+scarletfantasy@users.noreply.github.com>
2019-07-09 22:48:07 +0800

    Update script.sh

--- code/frontend/script.sh
+++ code/frontend/script.sh
@@ -1,8 +1,13 @@
-cd /frontend/SE231/code/frontend
+cd /frontend/SE231
 git pull origin
+cd code
+cd frontend
 sleep 10
+echo '1'
 sudo npm install
 sleep 30
+echo '2'
 sudo npm run build
 sleep 30
+echo '3'
 echo 'travis build done!'
