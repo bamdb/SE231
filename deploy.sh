a5f7aa6f18c0076903bf2add8c84efe8fea3c1fa
pleiadesian <wzl574402791@outlook.com>
2019-07-09 23:45:31 +0800

    fix bug in sh

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,54 @@
 #!/bin/sh
 ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
+git pull
+sleep 20
 cd config-server
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'config-server deployed'
 cd ../registry
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'registry deployed'
 cd ../user-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'user-service deployed'
 cd ../item-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'item-service deployed'
 cd ../activity-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'activity-service deployed'
 cd ../rating-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'rating-service deployed'
 cd ../topic-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'topic-service deployed'
 cd ../friend-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'friend-service deployed'
 cd ../comment-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'comment-service deployed'
 cd ../image-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'image-service deployed'
 cd ../message-service
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'message-service deployed'
 cd ../gateway
-sudo mvn spring-boot:run > bamdb.log &
+nohup mvn spring-boot:run > bamdb.log &
 sleep 10
+echo 'gateway deployed'
 cd ..
-exit 1
+echo 'deploy completed'
