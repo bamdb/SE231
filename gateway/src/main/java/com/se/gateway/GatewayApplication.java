65606b6c53454918efd0364134b594b4590c7f57
blasi7 <blasi@vip.qq.com>
2019-07-10 19:53:23 +0800

    password

--- gateway/src/main/java/com/se/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/se/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableZuulProxy
--- registry/src/main/java/com/se/registry/EurekaWebConfig.java
+++ registry/src/main/java/com/se/registry/EurekaWebConfig.java
@@ -1,4 +1,21 @@
 package com.se.registry;
 
-public class EurekaWebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class EurekaWebConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable(); //关闭csrf
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); //开启认证
+    }
+
 }
