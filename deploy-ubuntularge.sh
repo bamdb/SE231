533ec37e1fbf017d3bb0fd7be2c3c14a04783175
blasi7 <blasi@vip.qq.com>
2019-07-11 11:23:21 +0800

    fix deploy

--- deploy-ubuntularge.sh
+++ deploy-ubuntularge.sh
@@ -3,26 +3,26 @@ ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
 cd ~/SE231
 sleep 5
 cd config-server
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Dspring.profiles.active=ubuntularge > bamdb.log &
 sleep 50
 echo 'config-server deployed'
 cd ../registry
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Dspring.profiles.active=ubuntularge > bamdb.log &
 sleep 50
 echo 'registry deployed'
 cd ../user-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Dspring.profiles.active=ubuntularge > bamdb.log &
 sleep 5
 echo 'user-service deployed'
 cd ../item-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Dspring.profiles.active=ubuntularge > bamdb.log &
 sleep 5
 echo 'item-service deployed'
 cd ../image-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Dspring.profiles.active=ubuntularge > bamdb.log &
 sleep 5
 echo 'image-service deployed'
 cd ../gateway
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Dspring.profiles.active=ubuntularge > bamdb.log &
 sleep 5
 echo 'gateway deployed'
