55eada973a2d25ffd41777ca0d51a8baf349f80c
blasi7 <blasi@vip.qq.com>
2019-07-11 13:55:12 +0800

    sh

--- deploy-aliyun.sh
+++ deploy-aliyun.sh
@@ -2,49 +2,49 @@
 ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
 cd ~/bamdb/SE231
 cd config-server
-nohup mvn spring-boot:run > bamdb.log &
+nohup mvn clean spring-boot:run > bamdb.log &
 sleep 30
 echo 'config deployed'
 cd ../registry
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 10
 echo 'registry deployed'
 cd ../user-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'user-service deployed'
 cd ../item-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'item-service deployed'
 cd ../image-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'image-service deployed'
 cd ../activity-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'activity-service deployed'
 cd ../rating-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'rating-service deployed'
 cd ../topic-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 cd ../comment-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'comment deployed'
 cd ../message-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'message deployed'
 cd ../friend-service
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'friend deployed'
 cd ../gateway
-nohup mvn spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
+nohup mvn clean spring-boot:run -Dspring.profiles.active=aliyun > bamdb.log &
 sleep 5
 echo 'gateway deployed'
