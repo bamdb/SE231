aaad729018c6638ff84b51350ba15198b03dfcb0
blasi7 <blasi@vip.qq.com>
2019-07-11 11:02:15 +0800

    deploy

--- deploy-aliyun.sh
+++ deploy-aliyun.sh
@@ -2,45 +2,45 @@
 ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
 cd ~/bamdb/SE231
 cd registry
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 10
 echo 'registry deployed'
 cd ../user-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'user-service deployed'
 cd ../item-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'item-service deployed'
 cd ../image-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'image-service deployed'
 cd ../activity-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'activity-service deployed'
 cd ../rating-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'rating-service deployed'
 cd ../topic-service
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 cd ../comment
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'comment deployed'
 cd ../message
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'message deployed'
 cd ../friend
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'friend deployed'
 cd ../gateway
-nohup mvn spring-boot:run -Drun.profiles=ubuntularge > bamdb.log &
+nohup mvn spring-boot:run -Drun.profiles=aliyun > bamdb.log &
 sleep 5
 echo 'gateway deployed'
