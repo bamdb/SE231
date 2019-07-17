708d1ddb98f01d307436a5cf10d679ad03a50ecb
pleiadesian <wzl574402791@outlook.com>
2019-07-17 12:57:58 +0800

    roughly configured

---
+++ topic-service/src/main/java/com/se/topicservice/config/ds/DataSourceAspect.java
@@ -0,0 +1,44 @@
+package com.se.topicservice.config.ds;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.xml.crypto.Data;
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+public class DataSourceAspect {
+    @Before("@annotation(com.se.topicservice.config.ds.DataSource)")
+    public void beforeSwitchDS(JoinPoint point){
+
+        Class<?> className = point.getTarget().getClass();
+
+        String methodName = point.getSignature().getName();
+        Class[] argClass = ((MethodSignature)point.getSignature()).getParameterTypes();
+        String dataSource = DynamicDataSourceHolder.DEFAULT_DS;
+        try {
+            Method method = className.getMethod(methodName, argClass);
+
+            if (method.isAnnotationPresent(DataSource.class)) {
+                DataSource annotation = method.getAnnotation(DataSource.class);
+                dataSource = annotation.value();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        DynamicDataSourceHolder.putDataSource(dataSource);
+    }
+
+
+    @After("@annotation(com.se.topicservice.config.ds.DataSource)")
+    public void afterSwitchDS(JoinPoint point){
+        DynamicDataSourceHolder.clearDataSource();
+    }
+}
--- topic-service/src/main/java/com/se/topicservice/config/ds/DynamicDataSourceHolder.java
+++ topic-service/src/main/java/com/se/topicservice/config/ds/DynamicDataSourceHolder.java
@@ -1,14 +1,27 @@
 package com.se.topicservice.config.ds;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DynamicDataSourceHolder {
+    public static final Logger log = LoggerFactory.getLogger(DynamicDataSourceHolder.class);
+
+    public static final String DEFAULT_DS = "master";
 
     private static final ThreadLocal<String> holder = new ThreadLocal<>();
 
     public static void putDataSource(String dataSourceType) {
+        log.info("change data source to {}", dataSourceType);
         holder.set(dataSourceType);
     }
 
     public static String getDataSource() {
         return holder.get();
+    }
+
+    public static void clearDataSource() {
+        log.info("recover datasource");
+        holder.remove();
+        log.info("after recover datasource change to {}", holder.get());
     }
 }
