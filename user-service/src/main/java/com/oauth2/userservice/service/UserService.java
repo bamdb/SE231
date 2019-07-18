84ea9faa653b49e7bdfaaab53ff25fddeb5a41c2
blasi7 <blasi@vip.qq.com>
2019-07-19 01:04:29 +0800

    test

--- user-service/src/main/java/com/oauth2/userservice/service/UserService.java
+++ user-service/src/main/java/com/oauth2/userservice/service/UserService.java
@@ -2,9 +2,12 @@ package com.oauth2.userservice.service;
 
 import com.oauth2.userservice.domain.User;
 
+import javax.transaction.Transactional;
+
 public interface UserService {
     Iterable<User> selectAll();
     User selectByUsername(String username);
     User updateUser(User user);
+    @Transactional
     void deleteUserByUsername(String username);
 }
