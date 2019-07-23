d632ca99f4273e67411c445bf389c0daff40f2c7
pleiadesian <wzl574402791@outlook.com>
2019-07-23 11:35:28 +0800

    rm master backend

--- code/backend/comment-service/src/main/java/com/se/commentservice/CommentService.java
+++
@@ -1,9 +0,0 @@
-package com.se.commentservice;
-
-public interface CommentService {
-    Comment selectCommentByItemIdAndUserId(Long itemId, Long userId);
-    Iterable<Comment> selectCommentByItemId(Long itemId);
-    Iterable<Comment> selectCommentByUserId(Long userId);
-    Comment insertComment(Comment comment);
-    void deleteCommentByItemIdAndUserId(Long itemId, Long userId);
-}
