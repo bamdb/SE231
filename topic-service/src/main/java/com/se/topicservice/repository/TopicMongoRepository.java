2a88f0154b4f346ed02abd96a630e477dc3835de
pleiadesian <wzl574402791@outlook.com>
2019-07-23 11:34:43 +0800

    confirm

--- topic-service/src/main/java/com/se/topicservice/repository/TopicMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.se.topicservice.repository;
-
-import com.se.topicservice.entity.TopicPage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicMongoRepository extends CrudRepository<TopicPage, String> {
-}
