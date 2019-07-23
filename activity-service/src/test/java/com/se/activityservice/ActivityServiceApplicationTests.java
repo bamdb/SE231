ce8ec40dce830ce535a1ce271bdd47cb43de4c22
blasi7 <blasi@vip.qq.com>
2019-07-23 08:30:30 +0800

    activity tets

--- activity-service/src/test/java/com/se/activityservice/ActivityServiceApplicationTests.java
+++ activity-service/src/test/java/com/se/activityservice/ActivityServiceApplicationTests.java
@@ -6,6 +6,7 @@ import com.se.activityservice.client.ItemClient;
 import com.se.activityservice.client.UserClient;
 import com.se.activityservice.config.MethodSecurityConfig;
 import com.se.activityservice.config.ResourceServer;
+import com.se.activityservice.entity.MyPrincipal;
 import com.se.activityservice.service.impl.ActivityServiceImpl;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,6 +20,11 @@ import org.springframework.cloud.netflix.ribbon.StaticServerList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -28,6 +34,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -46,8 +55,19 @@ public class ActivityServiceApplicationTests {
     private WebApplicationContext context;
     private MockMvc mvc;
 
+    public Authentication authentication() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        MyPrincipal myPrincipal = new MyPrincipal();
+        myPrincipal.setAuthorities(authorities);
+        myPrincipal.setUsername("custom");
+        myPrincipal.setId(1L);
+        return new UsernamePasswordAuthenticationToken(myPrincipal, "customPassword", authorities);
+    }
+
     @Before
     public void setup() {
+        SecurityContextHolder.getContext().setAuthentication(authentication());
         mvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
@@ -199,7 +219,6 @@ public class ActivityServiceApplicationTests {
                 .andExpect(status().isOk());
         mvc.perform(get("/progress?itemId=1&userId=2")
                 .header("Authorization", "0"))
-
                 .andExpect(status().isOk());
     }
 
