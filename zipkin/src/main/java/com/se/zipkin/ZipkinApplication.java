671130690ca38e787262ae273911dea319ea9b7f
blasi7 <blasi@vip.qq.com>
2019-07-23 10:56:36 +0800

    auth

---
+++ zipkin/src/main/java/com/se/zipkin/ZipkinApplication.java
@@ -0,0 +1,15 @@
+package com.se.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}
