caf890ef170bc7a977877a3cff51a2b73cba5aee
shenruien <40877197+scarletfantasy@users.noreply.github.com>
2019-07-23 18:55:33 +0800

    delete

--- code/backend/topic-service/src/main/java/com/se/topicservice/repository/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package com.se.topicservice.repository;
-
-import com.se.topicservice.entity.Topic;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, Long> {
-
-}
