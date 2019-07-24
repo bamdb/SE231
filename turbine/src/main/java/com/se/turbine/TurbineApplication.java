d3aa4bf3bcbbb9e7b9eedd6f1cf61a8a501b3660
blasi7 <blasi@vip.qq.com>
2019-07-24 16:50:29 +0800

    user/all

---
+++ turbine/src/main/java/com/se/turbine/TurbineApplication.java
@@ -0,0 +1,13 @@
+package com.se.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+
+}
