0efadecdf2f064d150befe3a4c675aa4ff89b98f
root <blasi@vip.qq.com>
2019-07-25 23:58:33 +0800

    add rm stack

--- code/docker/composeup.sh
+++ code/docker/composeup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 docker-compose stop
+docker-compose -f docker-compose-config.yml stop
+docker-compose -f docker-compose-registry.yml stop
 docker-compose rm -f
+docker-compose -f docker-compose-config.yml rm -f
+docker-compose -f docker-compose-registry.yml rm -f
 docker-compose pull
+docker-compose -f docker-compose-config.yml pull
+docker-compose -f docker-compose-registry.yml pull
 docker-compose -f docker-compose-config.yml up -d
-sleep 20
+sleep 50
 echo "config up"
 docker-compose -f docker-compose-registry.yml up -d
-sleep 20
+sleep 50
 echo "registry up"
 docker-compose up -d
 echo "done"
