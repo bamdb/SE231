e286d98aa95c2bac47ef4c9b81e5b6b983bc810f
pleiadesian <wzl574402791@outlook.com>
2019-07-30 14:12:40 +0800

    simple web socket

---
+++ message-service/src/main/java/com/se/messageservice/socket/WebSocket.java
@@ -0,0 +1,51 @@
+package com.se.messageservice.socket;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+@ServerEndpoint("/websocket")
+@Slf4j
+public class WebSocket {
+    private Session session;
+
+    private static CopyOnWriteArraySet<WebSocket> webSockets = new CopyOnWriteArraySet<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        webSockets.add(this);
+        log.info("【websocket消息】有新的连接, 总数:{}", webSockets.size());
+    }
+
+
+    @OnClose
+    public void onClose() {
+        webSockets.remove(this);
+        log.info("【websocket消息】连接断开, 总数:{}", webSockets.size());
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        log.info("【websocket消息】收到客户端发来的消息:{}", message);
+    }
+
+    public void sendMessage(String message){
+        for (WebSocket webSocket: webSockets) {
+            log.info("【websocket消息】广播消息, message={}", message);
+            try {
+                webSocket.session.getBasicRemote().sendText(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
