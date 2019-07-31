f24d8592dd5d419c69d3c254be3b9c72dbf442cc
blasi7 <blasi@vip.qq.com>
2019-07-31 09:59:32 +0800

    finish elasticsearch

---
+++ search-service/src/main/java/com/se/searchservice/entity/User.java
@@ -0,0 +1,35 @@
+package com.se.searchservice.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "user", type = "doc")
+public class User {
+    @Id
+    Long id;
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
+    String username;
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
