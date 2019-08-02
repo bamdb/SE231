84c79f82344f5d911dc971220b0cbae8ae8211bd
YuyangHuang <50946857+YuyangHuang@users.noreply.github.com>
2019-08-02 15:29:38 +0800

    Update scheduletable.js

--- code/frontend/src/component/scheduletable.js
+++ code/frontend/src/component/scheduletable.js
@@ -147,7 +147,7 @@ class Scheduletable extends Component {
         axios.defaults.headers.common['Authorization'] = "Bearer "+localStorage.getItem("access_token");
 
         axios.get("http://202.120.40.8:30741/activity/progress", {params:{
-                    userId:this.props.userid,
+                    userId:localStorage.getItem("userid"),
                     itemId:this.props.itemid,
                     
                 }}).then(function(response)
@@ -159,9 +159,9 @@ class Scheduletable extends Component {
                 const length = (readdata===undefined) ? 0 :readdata.length ;
                 for (var i=0;i<length;++i){
                     console.log("child",readdata[i].sections.length)
-                    if(readdata[i].sections.length===0) //下面没有子章节了
+                    if(readdata[i].sections.length==0) //下面没有子章节了
                     {
-                        if(readdata[i].finish===1) {
+                        if(readdata[i].finish==true) {
                             treeData.push({
                                 title: "Node" +'-'+ (i + 1).toString(),
                                 value: '0-' + i.toString(),
@@ -184,7 +184,7 @@ class Scheduletable extends Component {
                         var read=true;
                         const sections=readdata[i].sections;
                         for(var j=0;j<sections.length;j++)
-                            if(sections[j]==1) {
+                            if(sections[j]==true) {
                                 children.push({
                                     title: "Node"+ parenttitle +'-'+ (j + 1).toString(),
                                     value: parentvalue + '-' + j.toString(),
