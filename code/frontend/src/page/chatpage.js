1eb8d122b033afe3b2176b1b8f3b8d286c62f5c6
pleiadesian <wzl574402791@outlook.com>
2019-08-19 14:48:24 +0800

    chatbox style completed

--- code/frontend/src/page/chatpage.js
+++ code/frontend/src/page/chatpage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Chat from 'chat-react';
+import Identicon from "identicon.js";
+import '../css/chatbox.css';
 
 class Chatpage extends Component {
     state = {
@@ -11,8 +13,17 @@ class Chatpage extends Component {
                         userId: "1544365758856"
                     },
                     value: "欢迎来到Bamdb聊天室"}],
-        timestamp: new Date().getTime()
+        timestamp: new Date().getTime(),
+        uuid: ""
     }
+    componentWillMount() {
+        this.setState({uuid : 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        })
+        });
+    }
+
     setInputfoucs = () => {
         this.chat.refs.input.inputFocus();  //set input foucus
     }
@@ -29,18 +40,21 @@ class Chatpage extends Component {
     render() {
         const {inputValue, messages, timestamp} = this.state;
         const islogin = (localStorage.getItem("userid")!=null);
+
+        var data = new Identicon(this.state.uuid, 420).toString();
         const userInfo = islogin? {
             avatar: "https://api.bamdb.cn/image/id/"+localStorage.getItem("userid")+"0",
             name: localStorage.getItem("username"),
             userId: localStorage.getItem("userid").toString()
         } : {
-            avatar: "https://api.bamdb.cn/image/id/"+localStorage.getItem("userid")+"0",
-            name: localStorage.getItem("username"),
-            userId: localStorage.getItem("userid").toString()
+            avatar: "data:image/png;base64," + data,
+            name: "匿名",
+            userId: 0
         }
         console.log(messages)
         return (
             <Chat
+                width="400px"
                 ref={el => this.chat = el}
                 className="my-chat-box"
                 dataSource={messages}
