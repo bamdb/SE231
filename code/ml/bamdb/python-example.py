bde2464e3f388b5d887f38b5e9862f5ff29298b2
shenruien <40877197+scarletfantasy@users.noreply.github.com>
2019-09-04 12:19:04 +0800

    add python-example

---
+++ code/ml/bamdb/python-example.py
@@ -0,0 +1,13 @@
+from tensorflow import keras
+from tensorflow.keras.models import model_from_json
+import numpy as np
+import json
+model=model_from_json(open('C:\\Users\\97023\\Desktop\\model\\model1.json').read())
+model.load_weights('C:\\Users\\97023\\Desktop\\model\\weight1.h5', by_name=True)
+input=np.zeros((1,5180))
+#  output is the possibility of each id whose shape is (,5180)
+output=np.argsort(model.predict(input))
+print(np.argsort(model.predict(input))[0][0:10])
+id2item=json.load(open('planb-id2item.json','r'))
+#  change the id into itemid
+print(id2item[str(output[0][0])])
